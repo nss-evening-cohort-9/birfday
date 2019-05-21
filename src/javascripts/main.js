b5ba4118833e96ac7361e1d2baec72f7efc13616
Zoe Ames <zoeames@gmail.com>
2019-05-20 19:54:54 -0500

    initialized firebase

--- src/javascripts/main.js
+++ src/javascripts/main.js
@@ -1,8 +1,14 @@
+import firebase from 'firebase/app';
+
 import Auth from './components/Auth/Auth';
 import Birfday from './components/Birfday/Birfday';
+
+import apiKeys from './helpers/apiKeys.json';
+
 import '../styles/main.scss';
 
 const init = () => {
+  firebase.initializeApp(apiKeys.firebaseKeys);
   Auth.authStringBuilder();
   Birfday.birfdayStringBuilder();
 };
